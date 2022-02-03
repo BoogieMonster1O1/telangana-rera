5b3649083aeeee970eb1f74a2194a1e51a5e863d
Prateek Deshmukh <prateek.deshmukh19@gmail.com>
2022-02-03 15:58:42 +0530

    add csv merge script

---
+++ merger.py
@@ -0,0 +1,11 @@
+import os
+import glob
+import pandas as pd
+os.chdir("/mydir")
+
+extension = 'csv'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+
+combined_csv.to_csv( "data.csv", index=False, encoding='utf-8-sig')
