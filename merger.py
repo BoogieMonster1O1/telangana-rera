511e9e5abd80dcff1cdebe6f5194b7449a6f79bd
SD <harrypotter.starwars.shrish@gmail.com>
2022-02-04 11:53:34 +0530

    new

--- merger.py
+++ merger.py
@@ -1,6 +1,6 @@
 import glob
 import pandas as pd
 
-combined_csv = pd.concat([pd.read_csv(f) for f in glob.glob('out/*.csv')])
+combined_csv = pd.concat([pd.read_csv(f) for f in glob.glob('old_out/*.csv')])
 
 combined_csv.to_csv("data.csv", index=False, encoding='utf-8-sig')
